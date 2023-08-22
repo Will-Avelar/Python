3c24903cb2cfe126024e3ac4f49db4622fadf502
Will-Avelar <willavelar@icloud.com>
2023-08-22 18:51:20 -0300

    001

---
+++ AprendendoPython.py
@@ -0,0 +1,28 @@
+print('Hello World!')
+#Pesquisar sobre: pypy, pep, 
+#
+
+
+#Variáveis simples 
+
+#string
+name = ("will")
+print(name)
+
+numero = 1
+numero = 2
+print (1 == 2)
+
+numero = 1 
+numero = 2 
+resultado = (1 < 2)
+print(resultado)
+
+
+
+nome = input ('Digite seu nome:' )
+cpf = input ('Digite seu CPF:' )
+endereco = input ('Digite seu Endereço:' )
+idade = input ('Digite sua idade: ')
+
+#Variáveis e tipos embutidos 
