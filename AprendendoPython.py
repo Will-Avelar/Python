d3007acc3d7c7de926e2603109c9e5ef56ab7c71
Will-Avelar <willavelar@icloud.com>
2023-08-24 22:12:59 -0300

    3º dia

--- AprendendoPython.py
+++ AprendendoPython.py
@@ -98,7 +98,7 @@ PS C:\Ws.will\Python>
 # Subtração -
 # Multiplicação *
 # Divisão /
-# POtênciação **
+# Potênciação **
 
 #Quando digitamos uma expressão o interpretador irá resolver e nos devolver o resultado 
 # Segue abeixo alguns exemplos 
@@ -125,4 +125,135 @@ print(3 // 2) # Nesse exemplo restou 1 esse 1 é o resto da divisão
 
 print(7 % 3)  # 7 dividido ppor 3 é igual a 2 o resto é o 1, esse operador é bem util quando queremos saber se um numero é divisivel por outro
 
+# O operador + tambem funciona com str de uma forna CONCATENADA
 
+texto1 = "Olá " 
+texto2 = "Python"
+print(texto1 + texto2)
+
+#O operador * tambem multiplica uma str
+
+texto1 = "Python "
+print(texto1 *10) #Ao multiplicar por 3 o python printa 3x a str (Caso não tivesse um espaço dentro da str as palavras ficariam grudadas)
+
+#Métodos de strings
+texto01 = "Python"
+texto01.upper() #Este método retorna a primeira letra em maiúscula 
+print(texto01)
+
+texto02 = 'python texto 02'
+texto2.capitalize
+print(texto02)
+
+#ENTRADA DE USUÁRIO
+#Python possui uma função que captura a entrada de valor essa função chama-se INPUT 
+
+nome = input ("Digite seu nome: ")
+
+#Podemos fazer uma formulário para ser preenchido
+#E para printar devemos passar todas as variáveis separadas por virgula
+nome = input ("Digite seu nome: ")
+idade = input ("Digite sua Idade: ")
+endereco = input ("Digite seu Endereço: ")
+cpf = input("Digite seu CPF: ")
+print(nome, idade, endereco, cpf)
+
+#Percebemos que as informação ao final não ficaram claras, pra isso devemos incluir str na função print com espaços 
+# e separando as variável das str usando o oprador + para concatenar
+nome = input ("Digite seu nome: \n")
+idade = input ("Digite sua Idade: \n")
+endereco = input ("Digite seu Endereço: \n")
+cpf = input("Digite seu CPF: \n")
+print(nome + " Tem " + idade + " anos de idade, mora na " + endereco + " e é portador do CPF " + cpf)
+
+#Outra maneira mais elegante de fazer é
+#dessa maneira o codigo fica mais limpo e ão é preciso quebrar a str em varias partes
+
+nome = input ("Digite seu nome: \n")
+idade = input ("Digite sua Idade: \n")
+endereco = input ("Digite seu Endereço: \n")
+cpf = input("Digite seu CPF: \n")
+print("{} Tem {} anos de idade, mora na {} e é portador do CPF {}" .format(nome, idade, endereco, cpf,))
+
+#Tambem podesmos usar a função format em numeros 
+x = 111.2345
+print('{:.2f}'.format(x)) #O :.2f arredonda diz que queremos apenas 2 casas decinais para a variável
+
+#CONSTANTES
+#True, False, None > essas tbm são palavras chave de python
+#True e False são valores Booleanos que representam verdadeiro ou falso 
+#python tbm tem a função bool() que retorna true ou false
+
+1 == '1'
+print (1 == '1')
+
+2 > 1
+print(2 > 1)
+
+2 < 1
+print(1 > 2)
+
+print(bool(5 > 3))
+
+print(bool(1 == 1))
+
+#Quando bool() não recebe uma expressão vai retornar true ou false
+print(bool(0))
+print(bool(''))
+print(bool(None))
+print(bool(1))
+print(bool(-190))
+print(bool(13.5))
+print(bool('Teste'))
+print(bool(False))
+
+#O none é um vaor do tipo nonetype é usado para representar uma abstenção de valor.
+type(None)
+print(type)
+
+#Operadores de comparação 
+# a == b         igual
+# a != b         diferente  
+# a < b          menor    
+# a > b          maior
+# a <= b         menor ou igual 
+# a => b         maor ou igual 
+
+#Outro operadores que retornam Boleanos não 
+
+# a is b         true se a e b forem identics 
+# a is not b     true se a e b não são identicos
+# a in b         true se a é membro de b 
+# a not in b     true se a não é menbro de b 
+
+#Importante lembra que == e is funcionam de maneira diferente
+
+x = [1, 2, 3]
+y = [1, 2, 3]
+print(x == y)
+
+x = [0, 2, 3]
+y = [1, 2, 3]
+print(x == y)
+
+
+x is x
+print(x is x)
+
+x is not x
+print(x is not x)
+
+
+#OPERADOR CONDICIONAL
+#COMANDO if
+
+
+numero = 42
+chute = input("Digite um numero: ")
+chute = int(chute) #neste caso estamos convertendo o valor de 42 para um numero inteiro qpe se não o pyhton vai entender que é uma str
+if numero == chute:
+  print("Vc Acertou")
+else:
+  print("Voce Errou.")
+  #Temos que tomar cuidade pq nem toda str pode ser convertida para int
+  
